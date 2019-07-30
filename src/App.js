006cd9d33d228843716aecec399072c8e64f95b2
irrino4ka <ir.rino4ka@gmail.com>
2019-07-30 23:56:47 +0300

    added first todo items

--- src/App.js
+++ src/App.js
@@ -6,14 +6,57 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      userName: "Ira"
+      userName: "Ira",
+      todoItems: [
+        {action: "Feed hamster", done: false},
+        {action: "Feed kids", done: false},
+        {action: "Finish react course", done: false},
+        {action: "Buy new bag", done: true},
+        {action: "Go to bed", done: false},
+      ],
+      newItemText: ""
     }
+  }
+
+  updateNewTextValue = (event) => {
+    this.setState({ newItemText: event.target.value});
+  }
+  createNewTodo = () => {
+    if (!this.state.todoItems
+        .find(item => item.action === this.state.newItemText)) {
+          this.setState({
+            todoItems: [
+              ...this.state.todoItems, 
+              {action: this.state.newItemText, done: false}
+            ],
+            newItemText: ""
+          })
+        }
+  }
+  changeStateData = () => {
+    this.setState({
+      userName: this.state.userName === 'Ira' ? 'Dima' : 'Ira'
+    })
   }
   render () {
     return (
       <div>
         <h4 className="bg-primary text-white text-center p-2">
-        {this.state.userName}'s To Do List</h4>
+        {this.state.userName}'s To Do List
+        ({this.state.todoItems.filter(el => !el.done).length} items to do)
+        </h4>
+        <div className = "content-fluid">
+          <div className = "my-1">
+              <input className = "form-control"
+              value= {this.state.newItemText}
+              onChange = {this.updateNewTextValue}
+              />
+              <button className="btn btn-primary m-2"
+              onClick ={this.createNewTodo}>
+                Add new
+              </button>
+          </div>
+        </div>
       </div>
     )
   }
